210dd53330fb97464a37c0b9d2aaad7b6f4d78e6
Sabely Sabely <enriquezsabely@gmail.com>
2026-08-31 11:26:31 -0400

    fixing and adding to the js

--- task.js
+++ task.js
@@ -2,9 +2,19 @@ const listView = document.querySelector("#list-view");
 const taskView = document.querySelector("#tasks-view");
 const newListView = document.querySelector("#new-list-view");
 
+const newListForm = document.querySelector("#new-list-form");
+const listContainer = document.querySelector("#list-container");
+const emptyListMessage = document.querySelector("#empty-list");
+const listNameInput = document.querySelector("#list-name");
+const listDeadlineInput = document.querySelector("#list-deadline");
+const currentList = document.querySelector("#current-list");
+
 const openNewListButton = document.querySelector("#open-new-list");
 const cancelNewListButton = document.querySelector("#cancel-new-list");
 const backToListsButton = document.querySelector("#back-to-lists");
+
+let lists = [];
+let activeList = null;
 
 
 function showListView() {
@@ -13,19 +23,64 @@ function showListView() {
     newListView.classList.add("view-hidden");
 }
 
-function showNewListView(){
+
+function showNewListView() {
     listView.classList.add("view-hidden");
     taskView.classList.add("view-hidden");
     newListView.classList.remove("view-hidden");
 }
 
-openNewListButton.addEventListener("click",() => {
+function showTaskView () {
+    listView.classList.add ("view-hidden");
+    newListView.classList.add("view-hidden");
+    taskView.classList.remove("view-hidden");
+}
+
+
+newListForm.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const listName = listNameInput.value.trim();
+    const listDeadline = listDeadlineInput.value;
+
+    if (listName === "") {
+        return;
+    }
+
+    const listCard = document.createElement("button");
+
+    listCard.classList.add("list-card");
+
+    listCard.textContent = listName;
+
+    listContainer.appendChild(listCard);
+
+    listCard.addEventListener("click", () => {
+        currentList.textContent = listName;
+        showTaskView();
+    });
+
+    emptyListMessage.style.display = "none";
+
+    listNameInput.value = "";
+    listDeadlineInput.value = "";
+
+    showListView();
+
+});
+
+
+
+openNewListButton.addEventListener("click", () => {
     showNewListView();
 });
+
 
 cancelNewListButton.addEventListener("click", () => {
     showListView();
 });
+
 
 backToListsButton.addEventListener("click", () => {
     showListView();
