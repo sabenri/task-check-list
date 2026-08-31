c1db6807dba6bb5f29a5eb93f85b69807d32c438
Sabely Sabely <enriquezsabely@gmail.com>
2026-08-31 08:44:24 -0400

    added some js and css

---
+++ task.js
@@ -0,0 +1,28 @@
+const listView = document.querySelector("#list-view");
+const taskView = document.querySelector("#tasks-view");
+const newListView = document.querySelector("new-list-view");
+
+const openNewListButton = document.querySelector("#open-new-list");
+const cancelNewListButton = document.querySelector("#cancel-new-list");
+const backToListsButton = document.querySelector("#back-to-lists");
+
+
+function showListView() {
+    listView.classList.remove("view-hidden");
+    tasksView.classList.add("view-hidden");
+    newListView.classList.add("view-hidden");
+}
+
+function showNewListView(){
+    listView.classList.add("view-hidden");
+    taskView.classList.add("view-hidden");
+    newListView.classList.remove("view-hidden");
+}
+
+openNewListButton.addEventListener("click",() => {
+    showListView();
+});
+
+cancelNewListButton.addEventListener("click", () => {
+    showListView();
+});
